95b47f11535682b82c866316cb5e49aa12bda8e0
amrzco <alexandremiraz@gmail.com>
2014-06-13 01:14:23 +0200

    Create firstscript.py
    Extremely simple script to get into Python Script Programming.

---
+++ firstscript.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+welcome = "¡Hola, este es mi primer script en Python!" 
+ask = "Por favor, introduce un string a rotar:"
+print welcome.title()
+string = raw_input(ask)
+print string[::-1]
+#---------------------
